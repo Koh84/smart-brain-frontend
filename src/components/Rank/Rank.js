fd014033df005e25c95e990dfbf13bd58c2aeede
Kelvin <koh84@hotmail.com>
2022-05-16 12:33:38 +0100

    added usage of aws lambda function to update emoji

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import 'tachyons'
 
-const Rank = ({name, entries}) => {
-    return (
-        <div>
-            <div className='green f3'>
-                {`${name}, your current entry count is...`}    
+class Rank extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            emoji: ''
+        }
+    }
+
+    componentDidMount() {
+        this.generateEmoji(this.props.entries)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.entries === this.props.entries && prevProps.name === this.props.name){
+            return null
+        }
+        this.generateEmoji(this.props.entries)
+    }
+
+    generateEmoji = (entries) => {
+        fetch(`https://j6wp3gpshl.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({emoji: data.input})
+        }).catch(console.log('error with emoji'));
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='green f3'>
+                    {`${this.props.name}, your current entry count is...`}    
+                </div>
+                <div className='red f1'>
+                    {this.props.entries}    
+                </div>
+                <div className='blue f1'>
+                    {`Rank Badge: ${this.state.emoji}`}    
+                </div>
             </div>
-            <div className='red f1'>
-                {entries}    
-            </div>
-        </div>
-    )
+        );
+    }  
 }
 
 export default Rank;
